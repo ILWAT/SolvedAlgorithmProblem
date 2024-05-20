a5251d39e0a39a23781328e9c5720a040e59ffe9
J.H. Moon <87518434+ILWAT@users.noreply.github.com>
2024-05-20 23:39:58 +0900

    [Silver I] Title: 카드 구매하기, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ Python/백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -7,11 +7,6 @@ for element in priceList:
 
 memoi = [0] * (n+1) #dp 테이블
 
-# 원하는 카드 갯수를 구매하는 방법
-# 1. 처음부터 갯수에 맞는 카드를 구매 
-# 2. 원하는 카드 갯수-1 & 1개 카드 구매
-# 3. 원하는 카드 갯수의 공약수 구매
-# 4. 1개 구매 (memoi[0]*n)
 # 상향식
 for i in range(1, n+1):
   for j in range(1, i+1):
