6312c51ea76445d558dc8a365233b78dd86409aa
J.H. Moon <87518434+ILWAT@users.noreply.github.com>
2024-05-20 23:38:39 +0900

    [Silver I] Title: 카드 구매하기, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ Python/백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,7 +1,11 @@
 n = int(input())
-price = list(map(int, input().split()))
+price = [0]
+priceList = list(map(int, input().split()))
 
-memoi = [0] * n #dp 테이블
+for element in priceList:
+  price.append(element)
+
+memoi = [0] * (n+1) #dp 테이블
 
 # 원하는 카드 갯수를 구매하는 방법
 # 1. 처음부터 갯수에 맞는 카드를 구매 
@@ -9,21 +13,10 @@ memoi = [0] * n #dp 테이블
 # 3. 원하는 카드 갯수의 공약수 구매
 # 4. 1개 구매 (memoi[0]*n)
 # 상향식
-for i in range(n):
-  memoi[i] = price[i]
-  for j in range(i-1, -1, -1):
-    if j == 0:
-      memoi[i] = max(memoi[i], memoi[j]*(i+1))
-      continue
-      
-    if (i+1) % (j+1) == 0:
-      memoi[i] = max(memoi[i], memoi[j] * ((i+1)//(j+1)))
-    
-    k = i-j-1
-    if k >= 0 and k <= j:
-      memoi[i] = max(memoi[i], memoi[j]+memoi[k])
+for i in range(1, n+1):
+  for j in range(1, i+1):
+    memoi[i] = max(memoi[i], memoi[i-j] + price[j])
 
     
       
-print(memoi[n-1])
-
+print(memoi[n])
