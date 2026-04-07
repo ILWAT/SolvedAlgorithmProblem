3e8af58c3e9265b06d767261f1c2b4ccdb9511a2
J.H. Moon <87518434+ILWAT@users.noreply.github.com>
2026-04-07 23:46:01 +0900

    [Silver V] Title: 최소공배수, Time: 100 ms, Memory: 14288 KB -BaekjoonHub

---
+++ Java/백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        long answer = (a * b) / gcd(a, b);
+        
+        System.out.println(answer);
+    }
+    
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
