c374310a998e911f98118c3dbfe8c3f9467561d3
J.H. Moon <87518434+ILWAT@users.noreply.github.com>
2026-07-12 23:26:33 +0900

    [level 2] Title: 숫자의 표현, Time: 0.82 ms, Memory: 56.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int total = 0;
+            for (int j = i; total < n; j++) {
+                total += j;
+            }
+            if (total == n) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
